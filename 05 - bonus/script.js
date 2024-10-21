ee254cf063935fe2a8927dcc72476ae840035a1a
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-21 16:53:14 +0200

    Bonus done!

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -8,7 +8,25 @@ e buonasera se è sera (oltre le 17)
 const name = "Mario";
 
 // Dichiara la funzione qui.
+//ELABORAZIONE CLASSICA
+function hello(name) {
+  //controllo se è prima delle 13
+  if (new Date().getHours() < 13) {
+    //ritorno buongiorno con il nome concatenato
+    return `Buongiorno ${name}`;
+    //ALTRIMENTI SE È PRIMA DELLE 17
+  } else if (new Date().getHours() < 17) {
+    //ritorno buon pomeriggio con il nome concatenato
+    return `Buon pomeriggio ${name}`;
+    //ALTRIMENTI
+  } else {
+    //ritorno buonasera con il nome concatenato
+    return `Buonasera ${name}`;
+  }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(hello(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
