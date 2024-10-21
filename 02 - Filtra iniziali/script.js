fcaab62aa1ce6d183245adfaf0684713a0454e3d
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-21 15:38:11 +0200

    Es 2 arrow function done

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,8 +4,9 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 const firstLetterArray = [];
 
 // Dichiara la funzione qui.
-//funzione che accetta come parametri un array di stringhe ed una lettera
-function firstLetter(array, letter) {
+
+//ELABORAZIONE ARROW FUNCTION
+const firstLetter = (array, letter) => {
   //PER i che parte da 0 fino ad arrivare alla lunghezza dell'indice massimo (lunghezza array -1 OPPURE minore di lunghezza array) aumenta i
   for (let i = 0; i < array.length; i++) {
     //SE la parola in posizione i inizia con la stessa lettera di "letter"
@@ -14,7 +15,20 @@ function firstLetter(array, letter) {
       firstLetterArray.push(array[i]);
     }
   }
-}
+};
+
+// ELABORAZIONE CLASSICA
+//funzione che accetta come parametri un array di stringhe ed una lettera
+//function firstLetter(array, letter) {
+//PER i che parte da 0 fino ad arrivare alla lunghezza dell'indice massimo (lunghezza array -1 OPPURE minore di lunghezza array) aumenta i
+//  for (let i = 0; i < array.length; i++) {
+//SE la parola in posizione i inizia con la stessa lettera di "letter"
+//    if (array[i][0] === letter) {
+// aggiungi nell'array firstLetterArray la parola
+//      firstLetterArray.push(array[i]);
+//    }
+//  }
+//}
 
 // Invoca la funzione qui e stampa il risultato in console
 firstLetter(names, "A");
